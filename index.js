30551f1608e1722c4dcf7d827cfde95aefef3ecf
RyanRJyeo <ryanrj@hotmail.sg>
2020-02-25 15:28:53 +0800

    graphql backend tutorial from Brad Traversy

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const expressGraphQL = require('express-graphql');
+const schema = require('./schema.js');
+
+
+const app = express();
+
+
+app.use('/graphql', expressGraphQL({
+  // schema:schema,
+  schema,
+  graphiql:true
+}));
+
+
+const port = 4000;
+app.listen(port, ()=>{
+  console.log(`Server is running on port (((((${port})))))`);
+});
